a625db2bf0e83f42ebfc0a8bebe859783a1f209b
Rafael Almeida <rafa261010@outlook.com>
2023-08-04 11:07:40 -0300

    initial commit

---
+++ exe003/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strings em PHP</title>
+</head>
+<body>
+    <h1>Strings em PHP</h1>
+    <?php
+        const ALVO = " PHP \u{1f418}";
+        
+        echo "Estudo " . ALVO;
+
+
+        echo " e estamos no ano de " . date('Y');
+    ?>
+</body>
+</html>

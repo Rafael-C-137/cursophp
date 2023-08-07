614cdafbd599070ce7642cd94b1ae41bb7ca3dac
Rafael Almeida <rafa261010@outlook.com>
2023-08-07 19:14:11 -0300

    criação do antecessor sucessor

---
+++ desafio/d001/resposta.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    
+    <main>
+        <h1>Resultado final</h1>
+        <?php
+            $n = $_GET["num"] ?? 0;
+            echo "<br>O número escolhido foi<strong>$n</strong>!";
+            echo "<br>O seu <em>antecessor<em> é <strong>". $n -1 ."</strong>!";
+            echo "<br>O seu <em>sucessor<em> é <strong>". $n + 1 ."</strong>!";
+        ?>
+        <button onclick="javascript:window.location.href='index.html'">
+        &#x2b05; Voltar</button>
+    </main>
+</body>
+</html>
